9db850ef5fd66dd7537d83941a6fb44a83ec1a26
KNJS0418 <klightjs@gmail.com>
2025-09-19 20:13:54 +0700

    type conversion complete

--- ex-1.js
+++ ex-1.js
@@ -2,7 +2,19 @@ let accountBalance = 400;
 let depositAmountFromUser = "10";
 
 accountBalance = accountBalance + depositAmountFromUser;
+console.log(accountBalance);
 
 let accountBalanceMessage = "Your account balance is " + accountBalance;
 
 console.log(accountBalanceMessage);
+
+depositAmountFromUser = Number(depositAmountFromUser);
+console.log(typeof depositAmountFromUser);
+
+accountBalance = 400;
+console.log(typeof accountBalance);
+
+accountBalance = accountBalance + depositAmountFromUser;
+accountBalanceMessage = "Your account balance is " + accountBalance;
+console.log(accountBalanceMessage);
+
